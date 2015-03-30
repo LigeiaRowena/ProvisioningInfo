4b0ab93684a0908a3363c67eb273ab832aa1f6fe
Francesca Corsini <francescacorsini@phxrmac01.local>
2015-03-30 09:41:00 +0200

    Initial Commit

---
+++ Provisioning_Info/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Provisioning_Info
+//
+//  Created by Francesca Corsini on 30/03/15.
+//  Copyright (c) 2015 Francesca Corsini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
